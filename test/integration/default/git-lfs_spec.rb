a82c5ae03c2b0bd105436f36dc88dcbdfb53b5a2
Derek Groh <derekgroh@gmail.com>
2020-12-30 16:25:37 -0600

    create ubuntu platform test

--- test/integration/default/git-lfs_spec.rb
+++ test/integration/default/git-lfs_spec.rb
@@ -1,3 +1,11 @@
-describe file('/etc/yum.repos.d/github_git-lfs.repo') do
-  it { should exist }
+if os['family'] == 'rhel'
+  describe file('/etc/yum.repos.d/github_git-lfs.repo') do
+    it { should exist }
+  end
+end
+
+if os['family'] == 'ubuntu'
+  describe file('/etc/apt/sources.list.d/github_git-lfs.repo') do
+    it { should exist }
+  end
 end
